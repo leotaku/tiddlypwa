16cff962a7abb90618a0d8fa54a2b8d1e6158a9f
Val Packett <val@packett.cool>
2023-07-10 02:39:28 -0300

    Server: add basic sync test

--- tiddlypwa-server.test.ts
+++ tiddlypwa-server.test.ts
@@ -29,6 +29,72 @@ const uploadAppFile = (token: string, body: string, extra?: Record<string, unkno
 		},
 	});
 
+type tidjson = {
+	thash: string;
+	title?: string;
+	tiv?: string;
+	data?: string;
+	iv?: string;
+	mtime?: Date;
+	deleted?: boolean;
+};
+const sync = (token: string, authcode: string, now: Date, lastSync: Date, clientChanges: Array<tidjson>) =>
+	api({ op: 'sync', token, authcode, now, lastSync, clientChanges });
+
+Deno.test('basic syncing works', async () => {
+	const tok = await createWiki();
+	const s1date = new Date();
+	// Basic write, with and without an mtime
+	assertEquals(
+		await sync(tok, 'test', s1date, new Date(0), [
+			{ thash: 'T3dP', data: '1111' },
+			{ thash: 'VXdV', data: '11111111', mtime: new Date(69) },
+		]),
+		{ appEtag: null, serverChanges: [] },
+	);
+	// Wrong authtok
+	assertEquals(await sync(tok, 'wrong', new Date(), new Date(), []), { error: 'EAUTH' });
+	// Basic reads
+	assertEquals(await sync(tok, 'test', new Date(), new Date(0), []), {
+		appEtag: null,
+		serverChanges: [
+			{
+				thash: 'T3dP',
+				title: null,
+				tiv: null,
+				data: '1111',
+				iv: null,
+				mtime: s1date.toISOString(),
+				deleted: false,
+			},
+			{
+				thash: 'VXdV',
+				title: null,
+				tiv: null,
+				data: '11111111',
+				iv: null,
+				mtime: new Date(69).toISOString(),
+				deleted: false,
+			},
+		],
+	});
+	assertEquals(await sync(tok, 'test', new Date(), new Date(420), []), {
+		appEtag: null,
+		serverChanges: [
+			{
+				thash: 'T3dP',
+				title: null,
+				tiv: null,
+				data: '1111',
+				iv: null,
+				mtime: s1date.toISOString(),
+				deleted: false,
+			},
+		],
+	});
+	await deleteWiki(tok);
+});
+
 // as we have a complex refcounting system for blobs, ensure they don't ever get left over
 Deno.test('app file blob garbage collected when changing content or deleting wiki', async () => {
 	const tok1 = await createWiki();
